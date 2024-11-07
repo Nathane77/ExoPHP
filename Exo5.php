b0b06e7d79dabcb92b36b7f850844b24a8c0c5fb
Nathane77 <hamidoulila14@gmail.com>
2024-11-07 16:46:52 +0100

    done to EXO6

--- Exo5.php
+++ Exo5.php
@@ -6,7 +6,9 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Exercise 5</h1>
+    <p>Ecrire un algorithme qui déclare une valeur en francs et qui la convertit en euros.
+    Attention, la valeur générée devra être arrondie à 2 décimales.</p>
 </body>
 <style>
     body{
@@ -15,3 +17,10 @@
     }
 </style>
 </html>
+<?php
+$changeRate = 4.5;
+$franc = 100;
+$euro = ($franc / $changeRate);
+
+
+echo round($euro, 2);
--- Exo6.php
+++ Exo6.php
@@ -6,7 +6,9 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Exercice 6</h1>
+    <p>Ecrire un algorithme permettant de calculer un montant de facture à régler à partir de la quantité
+    d’articles, son prix hors taxe et un taux de TVA (exprimé en décimal. Ex : 20 % -> 0.2)</p>
 </body>
 <style>
     body{
@@ -15,3 +17,19 @@
     }
 </style>
 </html>
+<?php
+
+
+function Calc_Prix(){
+    $prixU = 9.99;
+    $TVA = 0.2;
+    $quantité = 30;
+
+   $PrixTTC = ($prixU * $TVA);
+   echo "$PrixTTC<br>";
+   $prixTotal = ($PrixTTC * $quantité); 
+
+   echo "$prixTotal";
+}
+
+Calc_Prix();

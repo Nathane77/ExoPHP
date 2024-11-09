e3f85dd16c6a27c4af96e325c7272bf5307eb439
Nathane77 <hamidoulila14@gmail.com>
2024-11-09 15:11:25 +0100

    removed useless exo in P1 Done  Exp 02p2=>04p2

--- Part2/Exo02.php
+++ Part2/Exo02.php
@@ -6,15 +6,32 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Exercise 1</h1>
-    <p></p>
+    <h1>Exercise 2</h1>
+    <p>Réaliser un algorithme permettant d’afficher le tableau HTML suivant (notez que le nom du pays
+    s’affichera en majuscule et que le tableau est trié par ordre alphabétique du nom de pays) grâce à
+    une fonction personnalisée.</p>
 </body>
 <style>
     body{
         background: grey;
         color: white;
     }
+    table, th, td {
+  border:1px solid black;
+    }
+</style>
 </style>
 </html>
 
 <?php
+function afficherTableHTML(){
+    $capitales = ["France"=>"Paris","Allemagne"=>"Berlin","USA"=>"Washington","Italie"=>"Rome"];
+    ksort($capitales);
+    
+    echo "<table><tr><th>Pays</th><br><th>capital</th></tr>";
+    foreach($capitales as $pays => $ville){
+        echo "<tr><td>$pays</td><td>$ville</td></tr>";
+    }
+    echo "</table>";
+}
+afficherTableHTML();
--- Part2/Exo03.php
+++ Part2/Exo03.php
@@ -6,8 +6,9 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Exercise 1</h1>
-    <p></p>
+    <h1>Exercise 3</h1>
+    <p>Afficher un lien hypertexte permettant d’accéder au site d’Elan Formation. Le lien devra s’ouvrir
+    dans un nouvel onglet (_blank).</p>
 </body>
 <style>
     body{
@@ -16,5 +17,5 @@
     }
 </style>
 </html>
-
 <?php
+echo "<a href=https://elan-formation.fr/accueil target=_blank>Elan</a>";
